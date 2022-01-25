1d64d866f106038809351e86b850874e5c9b72c8
hiddenhelper <bountyhunter0308@hotmail.com>
2022-01-24 17:03:04 -0800

    display random images, close modal by only default close button, change the cursor

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 import { Modal } from "react-bootstrap";
 import styled from "styled-components";
@@ -47,9 +47,25 @@ const Button = styled.button`
   display: block;
 `;
 
+const customStyles = {
+  rows: {
+    style: {
+      cursor: 'pointer',
+    }
+  }
+}
+
 export const Dashboard = () => {
   const [modalShow, setModalShow] = useState(false);
-  const kittenData = kittenList.sort((a,b) => sortKitten(a,b));
+  const [imageURL, setImageURL] = useState("");
+  const kittenData = kittenList.sort((a, b) => sortKitten(a, b));
+
+  useEffect(() => {
+    if (modalShow) {
+      const imgURL = LoremPixel();
+      setImageURL(imgURL);
+    }
+  }, [modalShow]);
 
   const handleRowClicked = () => {
     setModalShow(true);
@@ -59,9 +75,10 @@ export const Dashboard = () => {
     setModalShow(false);
   }
 
-  const LoremPixel = ({ width = 300, height = 300 }) => {
-    const src = `https://placekitten.com/${width}/${height}?t=${Date.now()}`;
-    return <KittenImage src={src} alt='Placeholder' />;
+  const LoremPixel = (width = 300, height = 300) => {
+    const imageNum = new Date().getTime() % 16 + 1;
+    const src = `https://placekitten.com/${width}/${height}?image=${imageNum}`;
+    return src;
   }
 
   const customSort = (rows, selector, direction) =>
@@ -81,8 +98,8 @@ export const Dashboard = () => {
       return direction === 'desc' ? comparison * -1 : comparison;
     });
 
-  return (<div>
-    <DataTable
+  return (<>
+    {kittenData && <DataTable
       title="Kittens"
       columns={columns}
       data={kittenData}
@@ -90,12 +107,14 @@ export const Dashboard = () => {
       selectableRows
       onRowClicked={handleRowClicked}
       sortFunction={customSort}
-    />
+      customStyles={customStyles}
+    />}
 
     <Modal
       show={modalShow}
       onHide={handleCloseModal}
       centered
+      backdrop="static"
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
@@ -103,14 +122,14 @@ export const Dashboard = () => {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <LoremPixel />
+        <KittenImage src={imageURL} alt='Placeholder' />
       </Modal.Body>
-      <Modal.Footer>
+      {/* <Modal.Footer>
         <Button primary onClick={handleCloseModal}>OK</Button>
         <Button onClick={handleCloseModal}>Close</Button>
-      </Modal.Footer>
+      </Modal.Footer> */}
     </Modal>
-  </div>)
+  </>)
 }
 
 export default Dashboard;
